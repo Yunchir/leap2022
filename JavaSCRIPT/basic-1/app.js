e02a541c6ff212c38c4c7710f1a39d9f66f8ffc4
Morning <morning@admins-iMac.local>
2022-11-28 13:34:23 +0800

    js

---
+++ JavaSCRIPT/basic-1/app.js
@@ -0,0 +1,79 @@
+// let a = 15;
+// let b = 10;
+
+
+// let add = ( a + b);
+// let sub = ( a - b);
+// let multi = ( a * b);
+// let zipped = ( a / b);
+// let pors = ( a % b);
+
+// console.log("niilber = " + add);
+// console.log("ylgvar = " + sub);
+// console.log("urjver = " + multi);
+// console.log("noogdvor = " + zipped);
+// console.log("huvi = " + pors)
+
+// num1 = 168;
+// num2 = (num1 - (num1 % 10 )) / 10;
+// num3 = (num2 - (num2 % 10 )) / 10;
+
+// console.log(num3);
+
+
+// let num3 = parseFloat(prompt("Enter fist munber"));
+// let num4 = parseFloat(prompt("Enter second number"));
+// alert("2 toonii niilber ni: " + (num3 + num4));
+// alert("Ehnii toog 2 dahi toond huvaahad uldegdel n : " + (num3 + num4));
+
+// ****************JS Basic *******************
+
+// "Comments can make code readable"
+//Javascript lesson beging
+/* Comments can make code readable */
+let number = 7;
+let a = "17";
+let bool = true;
+let empty;
+let sad = null;
+
+console.log(empty);
+console.log(null);
+
+let fistName = "Yunchir";
+let lastName = "Ulaanbandi";
+let maritalStatus = "false";
+let country = "Mongolia";
+let age = "19";
+
+
+let fistName1 = "Ulaanbandi", lastName1 = "Yunden", maritalStatus1 = "true", country1 = "Mongolia", age1 = "54";
+
+let myAge = "19";
+let yourAge = "20";
+console.log(myAge);
+
+//Gurvaljingiin perimetriig oloh
+/*let aSide = parseFloat(prompt("Enter A side"));
+let bSide = parseFloat(prompt("Enter B side"));
+let cSide = parseFloat(prompt("Enter C side"));
+alert("p ni:" + (aSide + bSide + cSide));*/
+
+// Toirgiin S ol??
+/*let circleA = parseFloat(prompt("Enter radius"))
+let circleB = circleA * circleA * 3.14
+alert("toiurginn talbai ni: " + circleB)*/
+
+//m = y2-y1/x2-x1
+/*let y2 = 5
+let y1 = 2
+let x1 = 3
+let x2 = 2
+let m = y2-y1/x2-x1
+alert(m)*/
+
+//15 Gegabyte нь хэдэн биттэй тэнцүү вэ ?
+/*let GB = 15;
+let byt = 1073741824;
+alert(GB * byt)*/
+
