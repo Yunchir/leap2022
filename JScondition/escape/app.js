7b582fd00fb250a49a9e490e3c5b905594abd30d
Morning <morning@admins-iMac.local>
2023-01-05 09:17:19 +0800

    leap

--- JScondition/escape/app.js
+++
@@ -1,3 +0,0 @@
-let hi =
-  "Hello i am \"Name Here\" \n I am 'Age here' years old. \n This is the new line with tab and some backslashes.";
-console.log(hi);
---
+++ js_react/lesson_1/function.js
@@ -0,0 +1,5 @@
+const name = "Yunchir"
+
+let title = <h1>Welcome to {name}`s website</h1>
+
+ReactDOM.render(title, document.getElementById("root"))
